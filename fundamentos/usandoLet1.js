2b1fb709638c9820099f1436e568c5438d784e2d
Bruna Themoteo <brunathemoteo@gmail.com>
2020-08-06 13:42:59 -0300

    Conclusão - Seção 3: Aula 35

---
+++ fundamentos/usandoLet1.js
@@ -0,0 +1,11 @@
+var numero = 1
+
+{
+    var numero = 2
+    console.log('Dentro = ', numero)
+}
+
+console.log('Fora = ', numero)
+
+// VAR: possui escopo global e escopo de funçao
+// LET: possui escopo global, escopo de funçao e escopo de bloco

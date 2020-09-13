503cc06b23ff27a44dc7ea908e69fff6a0c158c3
Bruna Themoteo <brunathemoteo@gmail.com>
2020-09-12 22:48:36 -0300

    Conteudo de estruturas de controle e repeticao

---
+++ controle/for1.js
@@ -0,0 +1,15 @@
+let contador = 1
+
+while (contador <= 10) {
+    console.log(`Contador = ${contador}`)
+    contador++
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`i = ${i}`)
+}
+
+const notas = [3.4, 7.5, 9.2, 1.3, 10.0]
+for (let i = 0; i < notas.length; i++) {
+    console.log(`Nota ${notas[i]}`)
+}

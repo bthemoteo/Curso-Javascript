590fcea9c452d69b1ca056c3e43948e2761ca1b5
Bruna Themoteo <brunathemoteo@gmail.com>
2020-10-05 16:21:02 -0300

    Atualizando aulas assistidas

---
+++ funcao/arrowFunction1.js
@@ -0,0 +1,20 @@
+let dobro = function (a) {
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = (a) => 2 * a
+
+let ola = function () {
+    return 'Olá'
+}
+
+ola = () => {
+    return 'Olá'
+}
+
+ola = () => 'Olá'
+ola = _ => 'Olá'

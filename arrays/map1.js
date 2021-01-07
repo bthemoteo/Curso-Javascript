a836ed7e06af9d90c5b38879ab07215c7074fb15
Bruna Themoteo <brunathemoteo@gmail.com>
2021-01-07 15:32:55 -0300

    Estudo de array

---
+++ arrays/map1.js
@@ -0,0 +1,19 @@
+const nums = [1,2,3,4,5]
+
+//For com proposito
+let resultado = nums.map(function(e){
+    return e*2
+})
+
+console.log(resultado)
+
+const soma10 = e => e+10
+const triplo = e => e*3
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.',',')}`
+
+resultado = nums.map(soma10).map(triplo).map(paraDinheiro)
+console.log(resultado)
+
+// O método map() invoca a função callback passada por argumento 
+// para cada elemento do Array e devolve um novo Array como resultado.
+// Transforma um array em outro
